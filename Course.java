9010fe3f04602bce34dab1c1209ffc85918ddd10
Nimsara Jayarathna <nimsarajayarathna1@gmail.com>
2025-08-05 11:08:00 +0530

    Intial Comiit

---
+++ Course.java
@@ -0,0 +1,11 @@
+public class Course {
+    private String code;
+    private String title;
+
+    public Course(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    // Implement the methods below
+}
